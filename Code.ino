ae75cdf60519467180cd1b278a76ae1b6de99f44
matthewonye <matthewonye@gmail.com>
2025-12-11 22:43:26 -0500

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -1,14 +1,46 @@
-const int RELAY_PIN = 4; // Relay signal pin
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+const int SOIL_SENSOR_PIN = A0;
+const int RELAY_PIN = 4;
+
+int moistureThreshold = 500;
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, HIGH); // Keep relay OFF at startup
+  digitalWrite(RELAY_PIN, HIGH); // relay OFF
+
+  dht.begin();
+
+  Serial.println("Smart Plant Monitoring System Initialized");
 }
 
 void loop() {
-  digitalWrite(RELAY_PIN, LOW);  // Turn relay ON (pump runs)
-  delay(3000);                   // Run for 3 seconds
-  digitalWrite(RELAY_PIN, HIGH); // Turn relay OFF (pump stops)
-  delay(3000);                   // Wait 3 seconds
+  int soilValue = analogRead(SOIL_SENSOR_PIN);
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  Serial.print("Soil: ");
+  Serial.print(soilValue);
+  Serial.print(" | Temp: ");
+  Serial.print(temperature);
+  Serial.print(" °C | Humidity: ");
+  Serial.println(humidity);
+
+  if (soilValue > moistureThreshold) {
+    digitalWrite(RELAY_PIN, LOW); // pump ON
+    Serial.println("Pump ON");
+  } else {
+    digitalWrite(RELAY_PIN, HIGH); // pump OFF
+    Serial.println("Pump OFF");
+  }
+
+  delay(2000);
 }
 
